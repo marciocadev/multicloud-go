c9ae81ebedff8601195e04696ab1763977044fc6
Marcio Cruz de Almeida <marciocadev@gmail.com>
2025-04-08 16:03:51 -0300

    update

--- function/function.go
+++ function/function.go
@@ -2,10 +2,12 @@ package function
 
 import (
 	"context"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/marciocadev/multicloud-go/cloud"
+	"github.com/marciocadev/multicloud-go/function/aws"
 	"github.com/marciocadev/multicloud-go/function/event"
-	"github.com/marciocadev/multicloud-go/function/handler"
 )
 
 // func RunFunction(handlerFunc interface{}) {
@@ -19,10 +21,24 @@ import (
 
 // RunFunction inicia a função serverless
 func RunFunction(h func(context.Context, *event.CloudRequest) (*event.CloudResponse, error)) {
-	// Converter a função do usuário para o tipo HandlerFunc
-	handlerFunc := handler.HandlerFunc(h)
-	// Criar um wrapper que converte eventos AWS para nosso formato
-	wrapper := NewWrapper(handlerFunc)
-	// Iniciar a função Lambda
+
+	wrapper := func(ctx context.Context, rawEvent interface{}) (interface{}, error) {
+		provider := cloud.CloudProvider(os.Getenv("CLOUD_PROVIDER"))
+
+		switch provider {
+		case cloud.AWS:
+			// Criar um wrapper AWS (padrão)
+			wrapper := &aws.AWSWrapper{Handler: h}
+			// Processar o evento
+			return wrapper.Handle(ctx, rawEvent)
+		default:
+			// Criar um wrapper AWS (padrão)
+			wrapper := &aws.AWSWrapper{Handler: h}
+			// Processar o evento
+			return wrapper.Handle(ctx, rawEvent)
+
+		}
+	}
+
 	lambda.Start(wrapper)
 }
--- function/wrapper.go
+++ function/wrapper.go
@@ -2,11 +2,6 @@ package function
 
 import (
 	"context"
-	"os"
-
-	"github.com/marciocadev/multicloud-go/cloud"
-	"github.com/marciocadev/multicloud-go/function/aws"
-	"github.com/marciocadev/multicloud-go/function/handler"
 )
 
 // Wrapper é a interface para wrappers específicos de cada provedor
@@ -14,18 +9,18 @@ type Wrapper interface {
 	Handle(ctx context.Context, event interface{}) (interface{}, error)
 }
 
-// NewWrapper cria um novo wrapper baseado no provedor configurado
-func NewWrapper(handler handler.HandlerFunc) Wrapper {
-	provider := cloud.CloudProvider(os.Getenv("CLOUD_PROVIDER"))
+// // NewWrapper cria um novo wrapper baseado no provedor configurado
+// func NewWrapper(handler handler.HandlerFunc) Wrapper {
+// 	provider := cloud.CloudProvider(os.Getenv("CLOUD_PROVIDER"))
 
-	switch provider {
-	case cloud.AWS:
-		return &aws.AWSWrapper{Handler: handler}
-	case cloud.GCP:
-		return &aws.AWSWrapper{Handler: handler}
-		// gcp.GCPWrapper{Handler: handler}
-	default:
-		// Retorna AWS como padrão se não especificado
-		return &aws.AWSWrapper{Handler: handler}
-	}
-}
+// 	switch provider {
+// 	case cloud.AWS:
+// 		return &aws.AWSWrapper{Handler: handler}
+// 	case cloud.GCP:
+// 		return &aws.AWSWrapper{Handler: handler}
+// 		// gcp.GCPWrapper{Handler: handler}
+// 	default:
+// 		// Retorna AWS como padrão se não especificado
+// 		return &aws.AWSWrapper{Handler: handler}
+// 	}
+// }
