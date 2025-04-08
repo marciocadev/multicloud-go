9ff4ed53e6afc1d0c77c5e7915d65d88eb5049ab
Marcio Cruz de Almeida <marciocadev@gmail.com>
2025-04-08 14:57:24 -0300

    update

--- function/function.go
+++ function/function.go
@@ -4,14 +4,25 @@ import (
 	"context"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/marciocadev/multicloud-go/function/event"
 	"github.com/marciocadev/multicloud-go/function/handler"
 )
 
-func RunFunction(handlerFunc interface{}) {
-	myFunc := func(ctx context.Context, event interface{}) (interface{}, error) {
-		wrapper := NewWrapper(handlerFunc.(handler.HandlerFunc))
-		return wrapper.Handle(ctx, event)
-	}
+// func RunFunction(handlerFunc interface{}) {
+// 	myFunc := func(ctx context.Context, event interface{}) (interface{}, error) {
+// 		wrapper := NewWrapper(handlerFunc.(handler.HandlerFunc))
+// 		return wrapper.Handle(ctx, event)
+// 	}
 
-	lambda.Start(myFunc)
+// 	lambda.Start(myFunc)
+// }
+
+// RunFunction inicia a função serverless
+func RunFunction(h func(context.Context, *event.CloudRequest) (*event.CloudResponse, error)) {
+	// Converter a função do usuário para o tipo HandlerFunc
+	handlerFunc := handler.HandlerFunc(h)
+	// Criar um wrapper que converte eventos AWS para nosso formato
+	wrapper := NewWrapper(handlerFunc)
+	// Iniciar a função Lambda
+	lambda.Start(wrapper)
 }
--- function/handler/handler.go
+++ function/handler/handler.go
@@ -3,7 +3,7 @@ package handler
 import (
 	"context"
 
-	"github.com/marciocadev/multicloud-go/function/event" // Ensure this path is correct or replace with the actual package path
+	"github.com/marciocadev/multicloud-go/function/event"
 )
 
 // HandlerFunc é o tipo de função que processa eventos cloud
